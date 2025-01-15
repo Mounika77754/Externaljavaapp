ce26b599ddac6dcde2bb68b9f35d655e4619a0d1
Mounika77754 <mouni77754@gmail.com>
2025-01-15 14:12:37 +0530

    done

---
+++ src/main/java/com/app/external/ExernalJava/App.java
@@ -0,0 +1,13 @@
+package com.app.external.ExernalJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
